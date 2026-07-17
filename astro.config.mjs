fa3485ccd1a2f44e6c783fe284411dc7f3724c40
Shawshank_King <1456502265@qq.com>
2026-07-17 15:41:35 +0800

    修改站点根路径

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://www.hushustest.com",
+	site: "https://www.scandi-test.com",
 	integrations: [sitemap()],
 	adapter: cloudflare({
 		platformProxy: {
